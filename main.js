552d5e51b1882542aa8f113562b0adfa625b64e0
17 <ai9622398@gmail.com>
2025-10-13 18:57:49 +0300

    g

---
+++ main.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gustav = document.getElementById("gustav"); 
+    const gustavdialog = document.getElementById("gustav-dialog");
+    const walter = document.getElementById("walter");
+    const walterdialog = document.getElementById("walter-dialog")
+    const jessidialog = document.getElementById("jessi-dialog");
+    const jessi = document.getElementById("jessi");
+    
+    let clickcount = 0; 
+    jessi.addEventListener("click", () => {
+       clickcount++;
+       if(clickcount == 1) {
+        jessidialog.style.display = "block";   
+       }
+       if(clickcount == 2) {
+        jessidialog.style.display = "none";
+        clickcount = 0; 
+      }
+    });
+
+    let clickcount2 = 0; 
+    walter.addEventListener("click", () => {
+       clickcount2++;
+       if(clickcount2 == 1) {
+        walterdialog.style.display = "block";   
+       }
+       if(clickcount2 == 2) {
+        walterdialog.style.display = "none";   
+        clickcount2 = 0; 
+      }
+    });
+
+    let clickcount3 = 0; 
+    gustav.addEventListener("click", () => {
+       clickcount3++;
+       if(clickcount3 == 1) {
+        gustavdialog.style.display = "block";   
+       }
+       if(clickcount3 == 2) {
+        gustavdialog.style.display = "none";   
+        clickcount3 = 0; 
+      }
+    });
+
+});
